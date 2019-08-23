afbf71288f27a565f2d5e477e1617f0a478ea874
theburningmonk <theburningmonk@gmail.com>
2019-08-23 11:58:46 +0200

    feat: added check-balance

---
+++ functions/check-balance.js
@@ -0,0 +1,13 @@
+const uuid = require('uuid/v4')
+const { getAccount } = require('../lib/account')
+
+module.exports.handler = async (event) => {
+  const { accountId } = event.pathParameters
+
+  const { currentState } = await getAccount(accountId)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(currentState)
+  }
+}
